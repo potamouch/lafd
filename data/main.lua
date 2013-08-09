c6fadc4d8747c95ff70b8ec4353e432c8fd1de0b
Christopho <christopho@solarus-games.org>
2013-08-09 14:08:30 +0200

    Reimplement is_debug_enabled in Lua.

--- data/main.lua
+++ data/main.lua
@@ -2,11 +2,20 @@
 
 local console = sol.main.load_file("console")()
 
+local debug_enabled = false
+function sol.main.is_debug_enabled()
+  return debug_enabled
+end
+
 -- Event called when the program starts.
 function sol.main:on_started()
 
   -- Load built-in settings (audio volume, video mode, etc.).
   sol.main.load_settings()
+
+  -- If there is a file called "debug" in the write directory, enable debug mode.
+  debug_enabled = sol.main.load_file("debug") ~= nil
+
   -- Show the title menu initially.
   local title_screen = require("menus/title")
   sol.main:start_menu(title_screen:new())
