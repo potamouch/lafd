3289606ec284453707172eaf4b3feea4cc18eaba
Binbin <contact@zeldaforce.net>
2017-07-12 19:12:05 +0200

    Scripting Flagello cave

---
+++ data/enemies/keese.lua
@@ -0,0 +1,22 @@
+local enemy = ...
+
+-- Keese: a bat that sleeps until the hero gets close.
+
+function enemy:on_created()
+
+  self:create_sprite("enemies/" .. enemy:get_breed())
+  self:set_life(1)
+  self:set_damage(1)
+  local movement = sol.movement.create("random")
+  movement:set_speed(32)
+  movement:start(enemy)
+
+end
+
+--enemy:set_random_treasures(
+--  { "heart", 1 },
+ -- { "rupee", 1 },
+--  { "rupee", 2 }
+--)
+
+enemy:set_layer_independent_collisions(true)
--- data/maps/dungeons/1/1f.lua
+++ data/maps/dungeons/1/1f.lua
@@ -23,23 +23,40 @@ function map:on_opening_transition_finished(destination)
     game:start_dialog("dungeon_1")
 end
 
-local function enemy_in_group_1_dead(enemy)
-      if not map:has_entities("enemy_group_1") and not map:get_game():get_value("dungeon_1_small_keys") then
+
+-- Deaths
+
+-- Group 1
+local function dungeon_1_enemy_group_1_dead(enemy)
+      if not map:has_entities("dungeon_1_enemy_group_1") and not map:get_game():get_value("dungeon_1_small_key_1") then
            sol.audio.play_sound("secret_1")
            map:create_pickable{
                 treasure_name = "small_key",
                 treasure_variant = 1,
-                treasure_savegame_variable = "dungeon_1_small_keys",
+                treasure_savegame_variable = "dungeon_1_small_key_1",
                 x = 744,
                 y = 1384,
                 layer = 0
               }
       end
 end
-for enemy in map:get_entities("enemy_group_1") do
-  enemy.on_dead = enemy_in_group_1_dead
+for enemy in map:get_entities("dungeon_1_enemy_group_1") do
+  enemy.on_dead = dungeon_1_enemy_group_1_dead
 end
 
+-- Group 6
+local function dungeon_1_enemy_group_6_dead(enemy)
+      if not map:has_entities("dungeon_1_enemy_group_6") then
+           map:set_doors_open("dungeon_1_door_group_1", true)
+          sol.audio.play_sound("door_open")
+      end
+end
+for enemy in map:get_entities("dungeon_1_enemy_group_6") do
+  enemy.on_dead = dungeon_1_enemy_group_6_dead
+end
+
+
+-- Switchs
 
 function dungeon_1_switch_1:on_activated()
 
@@ -48,9 +65,31 @@ function dungeon_1_switch_1:on_activated()
       sprite = "entities/chest",
       treasure_name = "small_key",
       treasure_variant = 1,
-      treasure_savegame_variable = "dungeon_1_switch_1",
+      treasure_savegame_variable = "dungeon_1_small_key_2",
       x = 1224,
       y = 1112,
       layer = 0
     }
 end
+
+-- Sensors
+
+function dungeon_1_sensor_1:on_activated()
+
+  if map:has_entities("dungeon_1_enemy_group_6") then
+    map:set_doors_open("dungeon_1_door_group_1", false)
+    sol.audio.play_sound("door_closed")
+  end
+
+end
+
+function dungeon_1_sensor_2:on_activated()
+
+  if map:has_entities("dungeon_1_enemy_group_6") then
+    map:set_doors_open("dungeon_1_door_group_1", false)
+    sol.audio.play_sound("door_closed")
+  end
+
+end
+
+map:set_doors_open("dungeon_1_door_group_1", true)
