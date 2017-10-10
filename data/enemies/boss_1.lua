ff1dd29718d6e1146a47e9648c603832e4ee716a
Binbin <contact@zeldaforce.net>
2017-10-10 14:47:57 +0200

    Boss 1

--- data/enemies/boss_1.lua
+++ data/enemies/boss_1.lua
@@ -15,21 +15,19 @@ local hero = map:get_hero()
 local sprite_head
 local circle_x
 local circle_y
-local clockwise_buffer = nil
-local direction = 3
 local angle = 0
+local clockwise = nil
 
 -- Event called when the enemy is initialized.
 function enemy:on_created()
 
   local x, y, layer = enemy:get_position()
+  circle_x = x
+  circle_y = y
   sprite_head = enemy:create_sprite("enemies/" .. enemy:get_breed() .. "_head")
-  sprite_head:set_direction(direction)
   enemy:set_life(4)
   enemy:set_damage(1)
   enemy:set_traversable(true)
-  circle_x = x + 32
-  circle_y = y
 
 end
 
@@ -51,22 +49,21 @@ end
 function enemy:update_movement()
 
       local x, y, layer = enemy:get_position()
-      local clockwise = math.random(2)
-      if clockwise_buffer ~= clockwise then
-        clockwise_buffer = clockwise
+      local clockwise_new = math.random(2)
+      if clockwise_new ~= clockwise then
+        clockwise = clockwise_new
         local movement = sol.movement.create("circle")
         angle = angle + math.pi
         movement:set_radius(32)
-        movement:set_radius_speed(16)
-        movement:set_center(circle_x, circle_y)
+        movement:set_angle_speed(90)
         movement:set_initial_angle(angle)
+        circle_x, circle_y = x - (circle_x - x), y - (circle_y - y)
         if clockwise == 1 then
           movement:set_clockwise(true)
-          --movement:set_center(x + 16, y)
         else
           movement:set_clockwise(false)
-          --movement:set_center(x -16, y)
         end
+        movement:set_center(circle_x, circle_y)
         movement:start(enemy)
       end
      
