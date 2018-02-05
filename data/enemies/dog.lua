6c1a603bffe984eaa527aeb538ed5c779903f7e7
Binbin <contact@zeldaforce.net>
2018-02-05 22:28:44 +0100

    dog

--- data/enemies/dog.lua
+++ data/enemies/dog.lua
@@ -1,9 +1,6 @@
 local enemy = ...
 local map = enemy:get_map()
 
-local angry = false
-local num_times_hurt = 0
-
 function enemy:on_created()
 
   enemy:set_life(10000)
@@ -23,32 +20,18 @@ end
 
 function enemy:on_obstacle_reached(movement)
 
-  if not angry then
-    enemy:go_random()
-  else
-    enemy:go_angry()
-  end
+   enemy:go_random()
+
 end
 
 function enemy:on_restarted()
 
-  if angry then
-    enemy:go_angry()
-  else
     enemy:go_random()
-    sol.timer.start(enemy, 100, function()
-      if map.angry_chickens and not angry then
-        enemy:go_angry()
-        return false
-      end
-      return true  -- Repeat the timer.
-    end)
-  end
+   
 end
 
 function enemy:go_random()
 
-  angry = false
   local movement = sol.movement.create("random")
   movement:set_speed(32)
   movement:start(enemy)
@@ -69,9 +52,14 @@ end
 
 function enemy:on_hurt()
 
-  num_times_hurt = num_times_hurt + 1
-  if num_times_hurt == 3 and not map.angry_chickens then
-    -- Make all chickens of the map attack the hero.
-    map.angry_chickens = true
-  end
+  local game = map:get_game()
+  local hero = game:get_hero()
+  local direction4 = enemy:get_direction4_to(hero)
+  enemy:set_can_attack(true)
+  enemy:get_sprite():set_direction(direction4)
+  enemy:get_sprite():set_animation("angry")
+  local movement = sol.movement.create("target")
+  movement:set_speed(96)
+  movement:start(enemy)
+
 end
