529d983938e5ef531d2a563152603e21d7646ad3
Binbin <contact@zeldaforce.net>
2018-02-09 17:22:37 +0100

    Debug caves

--- data/maps/caves/forest/cave_1.lua
+++ data/maps/caves/forest/cave_1.lua
@@ -10,7 +10,6 @@
 local map = ...
 local game = map:get_game()
 local separator_manager = require("scripts/maps/separator_manager")
-local light_manager = require("scripts/maps/light_manager")
 local companion_manager = require("scripts/maps/companion_manager")
 
 separator_manager:manage_map(map)
@@ -18,7 +17,5 @@ separator_manager:manage_map(map)
 function map:on_started()
 
   companion_manager:init_map(map)
-  light_manager:init(map)
-  map:set_light(0)
 
 end
--- data/maps/caves/forest/cave_2.lua
+++ data/maps/caves/forest/cave_2.lua
@@ -1,4 +1,4 @@
--- Lua script of map caves/forest/cave_2.
+-- Lua script of map caves/forest/cave_1.
 -- This script is executed every time the hero enters this map.
 
 -- Feel free to modify the code below.
@@ -9,7 +9,10 @@
 
 local map = ...
 local game = map:get_game()
+local separator_manager = require("scripts/maps/separator_manager")
 local companion_manager = require("scripts/maps/companion_manager")
+
+separator_manager:manage_map(map)
 
 function map:on_started()
 
