2f1490adaf37b15d7f91a77eaf3170a3f0ad1833
Binbin <contact@zeldaforce.net>
2018-03-12 22:11:07 +0100

    Collapsing soil

--- data/entities/collapsing_soil.lua
+++ data/entities/collapsing_soil.lua
@@ -12,18 +12,28 @@ local entity = ...
 local game = entity:get_game()
 local map = entity:get_map()
 local hero = map:get_hero()
+local timer = nil
+local hero_is_on_soil = false
 
 -- Event called when the custom entity is initialized.
 function entity:on_created()
-
-  entity:set_modified_ground("traversable")
-  entity:add_collision_test("touching", function(soil, entity_test)
-      if entity_test:get_type() == "hero" then
-        sol.timer.start(self, 2000, function()
-          sol.audio.play_sound("ground_grumble")
-          self:remove()
+  
+  self:set_modified_ground("traversable")
+  sol.timer.start(self, 50, function()
+    local distance = self:get_distance(hero)
+    if distance <=8 then
+        if hero_is_on_soil == false then
+          hero_is_on_soil = true
+          sol.timer.start(self, 3000, function()
+            if hero_is_on_soil then
+              sol.audio.play_sound("ground_grumble")
+              self:remove()
+            end
         end)
       end
+    else
+      hero_is_on_soil = false
+    end
+    return true
   end)
-
 end
