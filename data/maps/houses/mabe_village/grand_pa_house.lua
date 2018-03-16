30bc4fa152a1d3fe786f3ee5d12a08276cfeadbc
Binbin <contact@zeldaforce.net>
2018-03-17 00:34:48 +0100

    Debug houses

--- data/maps/houses/mabe_village/grand_pa_house.lua
+++ data/maps/houses/mabe_village/grand_pa_house.lua
@@ -3,6 +3,7 @@
 -- Variables
 local map = ...
 local game = map:get_game()
+local hero = map:get_hero()
 local companion_manager = require("scripts/maps/companion_manager")
 
 -- Methods - Functions
@@ -23,6 +24,26 @@ function map:talk_to_grandpa()
 
 end
 
+function map:talk_to_phone() 
+
+  local phone = map:get_entity("phone")
+  local phone_sprite = phone:get_sprite()
+  phone_sprite:set_animation("calling")
+  hero:freeze()
+  hero:get_sprite():set_ignore_suspend(true)
+  hero:set_animation("pickup_phone", function()
+    hero:set_animation("calling")
+    game:start_dialog("maps.houses.phone_booth.1", function() 
+      hero:set_animation("hangup_phone", function()
+        hero:unfreeze()
+        phone_sprite:set_animation("stopped")
+        hero:get_sprite():set_ignore_suspend(false)
+      end)
+    end)
+  end)
+
+end
+
 
 -- Events
 
@@ -37,6 +58,12 @@ end
 function grandpa:on_interaction()
 
       map:talk_to_grandpa()
+
+end
+
+function phone_interaction:on_interaction()
+
+      map:talk_to_phone()
 
 end
 
