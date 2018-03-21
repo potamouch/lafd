cc32768cf835865366dec94b1e3ab72adca30cbc
Binbin <contact@zeldaforce.net>
2018-03-21 18:00:12 +0100

    Library

--- data/maps/houses/mabe_village/library.lua
+++ data/maps/houses/mabe_village/library.lua
@@ -9,6 +9,7 @@
 
 local map = ...
 local game = map:get_game()
+local hero = map:get_hero()
 local companion_manager = require("scripts/maps/companion_manager")
 
 function map:set_music()
@@ -26,7 +27,30 @@ function map:on_started()
 
   companion_manager:init_map(map)
   map:set_music()
-
+  -- Secret book
+  book_9:set_enabled(false)
+  if game:get_value("get_secret_book") then
+    book_9:set_enabled(true)
+    book_secret:set_enabled(false)
+  end
+  collision_book:add_collision_test("facing", function(entity, other, crystal_sprite, other_sprite)
+    if other:get_type() == 'hero' and hero:get_state() == "running" and game:get_value("get_secret_book") == nil then
+      sol.timer.start(map, 1000, function()
+        movement = sol.movement.create("jump")
+        movement:set_speed(100)
+        movement:set_distance(32)
+        movement:set_direction8(6)
+        movement:set_ignore_obstacles(true)
+        movement:start(book_secret, function()
+          sol.audio.play_sound("jump")
+          game:set_value("get_secret_book", true)
+          book_9:set_enabled(true)
+          book_secret:set_enabled(false)
+        end)
+      end)
+    end
+  end)
+  
 end
 
 -- Event called after the opening transition effect of the map,
@@ -108,6 +132,12 @@ end
 function book_8_interaction:on_interaction()
 
       map:open_book(8)
+
+end
+
+function book_9:on_interaction()
+
+      map:open_book(9)
 
 end
 
