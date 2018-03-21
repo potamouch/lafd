b711e3bf48ee98671a938c55386768a67755dfbd
Binbin <contact@zeldaforce.net>
2018-03-21 17:21:21 +0100

    Sand crab

--- data/enemies/sand_crab.lua
+++ data/enemies/sand_crab.lua
@@ -1,37 +1,13 @@
--- Lua script of enemy sand_crab.
--- This script is executed every time an enemy with this model is created.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
 local enemy = ...
-local game = enemy:get_game()
-local map = enemy:get_map()
-local hero = map:get_hero()
-local sprite
-local movement
 
--- Event called when the enemy is initialized.
-function enemy:on_created()
+local behavior = require("enemies/lib/towards_hero")
 
-  -- Initialize the properties of your enemy here,
-  -- like the sprite, the life and the damage.
-  sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
-  enemy:set_life(1)
-  enemy:set_damage(1)
-end
+local properties = {
+  sprite = "enemies/" .. enemy:get_breed(),
+  life = 2,
+  damage = 4,
+  normal_speed = 64,
+  faster_speed = 64,
+}
 
--- Event called when the enemy should start or restart its movements.
--- This is called for example after the enemy is created or after
--- it was hurt or immobilized.
-function enemy:on_restarted()
-
-  movement = sol.movement.create("target")
-  movement:set_target(hero)
-  movement:set_speed(48)
-  movement:start(enemy)
-end
+behavior:create(enemy, properties)
