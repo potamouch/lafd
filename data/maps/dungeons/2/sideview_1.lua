3607e5bc6e61e55964e3fb208672cbda2e359632
Binbin <contact@zeldaforce.net>
2018-03-22 06:51:28 +0100

    Sideview

--- data/maps/dungeons/2/sideview_1.lua
+++ data/maps/dungeons/2/sideview_1.lua
@@ -1,4 +1,4 @@
--- Lua script of map dungeons/2/sideview_3.
+-- Lua script of map dungeons/2/sideview_1.
 -- This script is executed every time the hero enters this map.
 
 -- Feel free to modify the code below.
@@ -13,8 +13,8 @@ local game = map:get_game()
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+    map:set_side_view(true)
+
 end
 
 -- Event called after the opening transition effect of the map,
