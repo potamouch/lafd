33ec69566dc5bbf9ac7691ce9c9025ab6e8f1b9b
Binbin <contact@zeldaforce.net>
2018-03-23 16:26:15 +0100

    Rolling bones

--- data/enemies/boss/rolling_bones.lua
+++ data/enemies/boss/rolling_bones.lua
@@ -23,50 +23,64 @@ local angle_enemy = math.pi / 4
 local spike_move = false
 local enemy_continue_move = false
 local angle_to_spike
+local enemy_step = 1
 -- Event called when the enemy is initialized.
 function enemy:on_created()
 
   enemy:set_hurt_style("boss")
   local x,y,layer = enemy:get_position()
   sprite_enemy = enemy:create_sprite("enemies/" .. enemy:get_breed())
+  sprite_enemy:set_direction(2)
   enemy:set_life(4)
   enemy:set_damage(1)
   spike = map:create_enemy{
     breed = enemy:get_breed() .. "/rolling_bones_spike",
     direction = 2,
     x = x - 64,
-    y = y,
+    y = y - 73,
     layer = layer
   }
+  local x_spike,y_spike,layer = spike:get_position()
   sprite_spike = spike:get_sprite()
+  angle_to_spike = sol.main.get_angle(x, x_spike, y, y_spike + 73)
   sol.timer.start(spike, 150, function()
     if spike_move then
       sol.audio.play_sound("rolling_spike")
     end
     return true
   end)
+  enemy:go_to_spike()
 
 end
 
 function enemy:on_restarted()
 
-  sprite_spike:set_animation("stopped")
-  enemy:go_to_spike()
+  if step == 1 then
+    enemy:go_on_the_other_side()
+  elseif step == 2 then
+    enemy:push_spike()
+  else 
+    enemy:go_to_spike()
+  end
 
 end
 
 function enemy:go_to_spike()
-
+  
+  enemy_step = 1
+  local spike_x, spike_y = spike:get_position()
+  local direction = sprite_enemy:get_direction()
+  if direction == 0 then
+    spike_x = spike_x - 32
+  else
+    spike_x = spike_x + 32
+  end
   movement_enemy = sol.movement.create("target")
-  movement_enemy:set_target(spike)
+  movement_enemy:set_target(spike_x, spike_y + 73)
   movement_enemy:set_speed(64)
   movement_enemy:start(enemy)
   function movement_enemy:on_finished()
     movement_enemy:stop()
-    sol.timer.start(enemy, 1000, function()
-      angle_to_spike = enemy:get_angle(spike)
-      enemy:go_on_the_other_side()
-    end)
     enemy:push_spike()
   end
 
@@ -74,28 +88,40 @@ end
 
 function enemy:push_spike()
 
-    movement_spike = sol.movement.create("straight")
-    movement_spike:set_speed(96)
-    movement_spike:set_angle(direction_spike)
-    movement_spike:set_max_distance(200)
-    sprite_spike:set_animation("walking")
-    spike_move = true
-    function movement_spike:on_obstacle_reached()
-        direction_spike = math.pi - direction_spike
+    enemy_step = 2
+    sol.audio.play_sound("bounce")
+    sprite_enemy:set_animation("punching")
+    function sprite_enemy:on_animation_finished(animation)
+      if animation == "punching" then
+        sprite_enemy:set_animation("walking")
+        movement_spike = sol.movement.create("straight")
+        movement_spike:set_speed(96)
         movement_spike:set_angle(direction_spike)
-        movement_spike:set_max_distance(32)
+        movement_spike:set_max_distance(200)
+        sprite_spike:set_animation("walking")
+        spike_move = true
+        function movement_spike:on_obstacle_reached()
+            direction_spike = math.pi - direction_spike
+            movement_spike:set_angle(direction_spike)
+            movement_spike:set_max_distance(64)
+        end
+        function movement_spike:on_finished()
+          sprite_spike:set_animation("stopped")
+          spike_move = false
+        end
+        movement_spike:start(spike)
+        sol.timer.start(enemy, 1000, function()
+          angle_to_spike = enemy:get_angle(spike)
+          enemy:go_on_the_other_side()
+        end)
+      end
     end
-    function movement_spike:on_finished()
-      sprite_spike:set_animation("stopped")
-      spike_move = false
-    end
-    movement_spike:start(spike)
 
 end
 
 function enemy:go_on_the_other_side()
 
-  --angle_enemy = math.pi - angle_enemy
+  enemy_step = 1
   enemy_continue_move = true
   if angle_enemy ==  angle_to_spike - math.pi / 4 then
     angle_enemy = angle_to_spike + math.pi / 4
