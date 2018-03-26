4044b28bda8795a19f5477b011f9e909dcc73b2b
Zeldo <shadar669@gmail.com>
2018-03-26 19:34:28 +0200

    Cavernes de la montagnes qui forment enfin le réseau complet. Reste plus qu'à les remplir...

--- data/maps/out/d1_east_mt_tamaranch.lua
+++ data/maps/out/d1_east_mt_tamaranch.lua
@@ -62,3 +62,6 @@ auto_separator_1:register_event("on_activating", function(separator, direction4)
   end
 
 end)
+
+--Weak doors play secret sound on opened
+function weak_door_1:on_opened() sol.audio.play_sound("secret_1") end
